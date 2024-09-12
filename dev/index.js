4abcce54f88d6b56ebf6d515d82c98a88726c3a8
Aman Namdev <107068437+amannamdev07@users.noreply.github.com>
2024-09-12 15:18:18 +0530

    some changes made to index.html file

--- dev/index.js
+++ dev/index.js
@@ -1,9 +1,13 @@
 const hrs = document.getElementById('hrs');
 const min = document.getElementById('min');
 const sec = document.getElementById('sec');
+const curDate = document.getElementById('curDate');
 
 setInterval(() => {
     const date = new Date();
+
+    curDate.innerText = date.toLocaleDateString()
+
     hrs.innerText = String(date.getHours()).padStart(2, '0');  // Format hours as 2 digits
     min.innerText = String(date.getMinutes()).padStart(2, '0'); // Format minutes as 2 digits
     sec.innerText = String(date.getSeconds()).padStart(2, '0'); // Format seconds as 2 digits
